7776311cb942c979891dd7c16f3b57b9d206ab19
Julia Soares <jusoaresbtu@gmail.com>
2021-12-11 12:06:12 -0300

    Lógica de validação de campos e estilo da pagina_novo_lancamento

---
+++ pagina_novo_lancamento/script.js
@@ -0,0 +1,49 @@
+const btn = document.querySelector("#send");
+
+btn.addEventListener("click", function(event){
+
+    event.preventDefault();
+
+    const selecionaTitulo = document.querySelector("#titulo");
+    const selecionaValor = document.querySelector("#valorLancamento");
+    const selecionaData = document.querySelector("#data");
+ 
+
+    const titulo = selecionaTitulo.value;
+    const valor = selecionaValor.value;
+    const data = selecionaData.value;
+
+
+    validaVazio(titulo, valor, data);
+    validaNumero(valor)
+
+})
+
+
+function validaVazio(titulo, valor, data){
+    if(titulo.length == 0 || 
+        valor.length == 0 ||
+        data.length == 0 ){
+        return alert("Por favor preencha TODOS os campos antes de enviar!")
+    }else { return }
+}
+
+function validaNumero(valor){
+    if(isNaN(valor)){
+        return valorInvalido()
+    }
+}
+
+
+function valorInvalido(){
+    const selecionaMensagem = document.querySelector('#mensagemErro');
+    selecionaMensagem.innerText = ("");
+    const selcionaErro = document.querySelector('#mensagemErro');
+    selcionaErro.innerText = ("Valor inválido!");
+}
+
+// function positivoNegativo(valor){
+//     if( ){
+
+//     }
+// }
