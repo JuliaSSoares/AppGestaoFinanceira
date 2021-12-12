bacff47b4264106cf9ad49aed9375c7d8654abad
Julia Soares <jusoaresbtu@gmail.com>
2021-12-12 16:19:08 -0300

    linkando css

--- pagina_meus_lancamentos/pagina_meus_lancamentos.js
+++ pagina_meus_lancamentos/pagina_meus_lancamentos.js
@@ -1,39 +1,16 @@
 
-// const linha = document.querySelectorAll('tr')
 
-// console.log(coluna);
-
-// const colunaValue = coluna.innerHTML;
-
-// console.log(colunaValue);
-
-// if (colunaValue > 0){
-//     (coluna).classList.add('green');
-// }
-// else{
-//     (coluna).classList.add('red');
-
-// }
+let linha = document.querySelector('tbody tr:nth-child(1)'); console.log(linha)
+const valor =  ((document.querySelector('tr:nth-child(1) td:nth-child(2)')).innerHTML).replace(",","."); console.log(valor)
 
 
-function mudaCor(){
-    const coluna=  document.querySelectorAll('.tabela td:nth-child(2)');
-    const coluna=  document.querySelector('.tabela td:nth-child(2)');
-    const colunaValue = coluna.Value;
-    for( let i = 0; i < coluna.length; i++ ){
-        
-        console.log(colunaValue);
-
-        if (colunaValue > 0){
-            (coluna[i]).classList.add('green');
-        }
-        else{
-            (coluna[i]).classList.add('red');
-
-        }
+if(valor < 0){
+    linha.classList.add('red');
+} else{ 
+    linha.classList.add('green')
+}
 
 
-    }
-};
 
-mudaCor();
+const nLinhas = (document.querySelectorAll('tbody tr')).length ; console.log(nLinhas)
+
