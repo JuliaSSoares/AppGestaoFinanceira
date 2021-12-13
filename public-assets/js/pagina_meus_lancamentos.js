211e47525fdae43852e5e80b600b7354c5437a38
Julia Soares <jusoaresbtu@gmail.com>
2021-12-13 19:27:31 -0300

    commit

--- public-assets/js/pagina_meus_lancamentos.js
+++ public-assets/js/pagina_meus_lancamentos.js
@@ -1,7 +1,7 @@
 
 
-let linha = document.querySelector('tbody tr:nth-child(2)'); console.log(linha)
-const valor =  ((document.querySelector('tr:nth-child(2) td:nth-child(2)')).innerHTML).replace(",","."); console.log(valor)
+let linha = document.querySelector('tbody tr:nth-child(1)'); console.log(linha)
+let valor =  ((document.querySelector('tr:nth-child(1) td:nth-child(2)')).innerHTML).replace(",","."); console.log(valor)
 
 
 if(valor < 0){
@@ -14,3 +14,17 @@ if(valor < 0){
 
 const nLinhas = (document.querySelectorAll('tbody tr')).length ; console.log(nLinhas)
 
+function mudaCor(){
+    let linha = document.querySelector('tbody tr:nth-child(1)');
+    let valor =  ((document.querySelector('tr:nth-child(1) td:nth-child(2)')).innerHTML).replace(",","."); 
+    const nLinhas = (document.querySelectorAll('tbody tr')).length ; console.log(nLinhas)
+    for(let i = 0; i < nLinhas; i++){
+        if(valor < 0){
+            linha.classList.add('red');
+        } else{ 
+            linha.classList.add('green')
+        }
+         linha = document.querySelector('tbody tr:nth-child(i)');
+         valor =  ((document.querySelector('tr:nth-child(i) td:nth-child(2)')).innerHTML)
+    }
+}
