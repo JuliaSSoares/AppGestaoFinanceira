6b1ac883c49a77ebfb0b0f986acec7b5d71f1432
HelenaCFH <93144382+HelenaCFH@users.noreply.github.com>
2021-12-13 22:20:09 -0300

    Páginas linkadas ao index.html a partir do server.js

--- server.js
+++ server.js
@@ -12,10 +12,14 @@ app.engine("html", require("hbs").__express)
 app.set("views", __dirname + "/src") //indica qual o diretório
 
 //CONFIGURAÇÃO DOS CONTEÚDOS ESTÁTICOS
-//app.use(express.static(__dirname + "./novo_lancamento.css/assets/public"))
+app.use(express.static(__dirname + "/public-assets"))
 
 //Rotas da aplicação
-app.get('/', (req, res) => res.render("home")) 
+app.get('/', (req, res) => res.render("../index"))
+app.get('/cadastro', (req, res) => res.render("cadastro"))
+app.get('/login', (req, res) => res.render("login"))
+app.get('/novo_lancamento', (req, res) => res.render("novo_lancamento"))
+app.get('/meus_lancamentos', (req, res) => res.render("meus_lancamentos"))
 
 
 //Definindo a porta 3000 como porta de escuta
