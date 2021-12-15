dba1f80cc5a4c29c99239acfdc106dda7b80353b
HelenaCFH <93144382+HelenaCFH@users.noreply.github.com>
2021-12-15 04:17:45 -0300

    Iniciei o POST no router.js das páginas que precisarão disso.

--- src/routers.js
+++ src/routers.js
@@ -6,9 +6,16 @@ const router = express.Router()
 
 
 router.get('/', (req, res) => res.render("../../index"))
+
 router.get('/cadastro', (req, res) => res.render("cadastro"))
+router.post('/cadastro', (req, res) => res.render("cadastro"))
+
 router.get('/login', (req, res) => res.render("login"))
+router.post('/login', (req, res) => res.render("login"))
+
 router.get('/novolancamento', (req, res) => res.render("novo_lancamento"))
+router.post('/novolancamento', (req, res) => res.render("novo_lancamento"))
+
 router.get('/meuslancamentos', (req, res) => res.render("meus_lancamentos"))
 //router.post('/novo_lancamento', (req, res) => res.render("novo_lancamento"))
 
