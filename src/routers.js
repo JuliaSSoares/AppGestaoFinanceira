aacce9a4279056eccb572c7d784346937c234d5a
HelenaCFH <93144382+HelenaCFH@users.noreply.github.com>
2021-12-15 06:10:53 -0300

    Inserido código e arquivos necessários para conectar ao banco de dados

--- src/routers.js
+++ src/routers.js
@@ -1,9 +1,21 @@
 const express = require("express")
 const router = express.Router()
-
+const sqlite3 = require("sqlite3").verbose()
 
 //Acesso ao banco de dados
+const db = new sqlite3.Database(
+    "./database/database.db",
+    sqlite3.OPEN_READWRITE,
 
+    err => {
+        if (err) {
+            console.log(err.message)
+            return 
+        }
+        else
+        {console.log("conectado com sucesso ao BD")}
+    }
+)
 
 router.get('/', (req, res) => res.render("../../index"))
 
@@ -13,10 +25,10 @@ router.post('/cadastro', (req, res) => res.render("cadastro"))
 router.get('/login', (req, res) => res.render("login"))
 router.post('/login', (req, res) => res.render("login"))
 
-router.get('/novolancamento', (req, res) => res.render("novo_lancamento"))
-router.post('/novolancamento', (req, res) => res.render("novo_lancamento"))
+router.get('/novo_lancamento', (req, res) => res.render("novo_lancamento"))
+router.post('/novo_lancamento', (req, res) => res.render("novo_lancamento"))
 
-router.get('/meuslancamentos', (req, res) => res.render("meus_lancamentos"))
-//router.post('/novo_lancamento', (req, res) => res.render("novo_lancamento"))
+router.get('/meus_lancamentos', (req, res) => res.render("meus_lancamentos"))
+router.post('/meus_lancamentos', (req, res) => res.render("meus_lancamentos"))
 
 module.exports = router
