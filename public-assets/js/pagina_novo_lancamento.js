5d38514476c020951a28437af660ca9074ad4dc6
Julia Soares <jusoaresbtu@gmail.com>
2021-12-17 19:10:08 -0300

    Mudança alterando entrada de valores

--- public-assets/js/pagina_novo_lancamento.js
+++ public-assets/js/pagina_novo_lancamento.js
@@ -1,28 +1,3 @@
 $("#datepicker").datepicker({ dateFormat: "dd/mm/yy" }).val();
 
-
-const btn = document.querySelector("#send");
-
-btn.addEventListener("click", function (event) {
-  const selecionaValor = document.querySelector("#valorLancamento");
-  const valor = selecionaValor.value;
-
-  validaNumero(event, valor);
-});
-
-
-
-function validaNumero(event, valor) {
-  if (isNaN(valor)) {
-    event.preventDefault();
-    return valorInvalido();
-  }
-}
-
-function valorInvalido() {
-  const selecionaMensagem = document.querySelector("#mensagemErro");
-  selecionaMensagem.innerText = "";
-  const selcionaErro = document.querySelector("#mensagemErro");
-  selcionaErro.innerText = "Valor inválido!";
-}
-
+ 
