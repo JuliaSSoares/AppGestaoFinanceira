35173d76c43ebfcee5f47a616ffdab718a3deeaa
unknown <ruanmarques.eng@gmail.com>
2021-12-19 18:06:38 -0300

    atualizacao cogido de seguranca

--- segurança/codigo_seguranca_token.php
+++ segurança/codigo_seguranca_token.php
@@ -2,12 +2,11 @@
 
 
 
-function base64UrlEncode($data)
-{
+function base64UrlEncode($data){
     return str_replace(['+', '/', '='], ['-','_',''], base64_encode($data));
 }
-$header = base64UrlEncode( '{"alg": "HS256", "typ": "JWT"}' );
-$payload = base64UrlEncode( '{"sub": "'.  md5(time()) .'", "name:" "Greenomac Dias", "iat": '.  time() . '}' );
+$header = base64UrlEncode('{"alg": "HS256", "typ": "JWT"}');
+$payload = base64UrlEncode('{"sub": "'.  md5(time()).'", "name": "Greenomac Dias", "iat": '.  time() . '}' );
 $signature = base64UrlEncode(
         hash_hmac('sha256', $header.'.'.$payload, 'segredo', true)
         );
