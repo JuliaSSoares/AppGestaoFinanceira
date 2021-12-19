570a0aa93f0b8b084bf17cc80e82f55e2c672d8e
unknown <ruanmarques.eng@gmail.com>
2021-12-19 18:08:33 -0300

    atualizacao seguranca

--- segurança/codigo_seguranca_token.php
+++ segurança/codigo_seguranca_token.php
@@ -6,8 +6,8 @@ function base64UrlEncode($data){
     return str_replace(['+', '/', '='], ['-','_',''], base64_encode($data));
 }
 $header = base64UrlEncode('{"alg": "HS256", "typ": "JWT"}');
-$payload = base64UrlEncode('{"sub": "'.  md5(time()).'", "name": "Greenomac Dias", "iat": '.  time() . '}' );
+$payload = base64UrlEncode('{"sub": "'.  md5(time()).'", "name": "Ruan Marques", "iat": '.time().'}');
 $signature = base64UrlEncode(
         hash_hmac('sha256', $header.'.'.$payload, 'segredo', true)
-        );
+);
 echo $header.'.'.$payload.'.'.$signature;
