1f78898a9c61f09e399fc7db16b7f2dbe1779776
unknown <ruanmarques.eng@gmail.com>
2021-12-20 17:30:40 -0300

    ruan

--- public-assets/js/pagina_meus_lancamentos.js
+++ public-assets/js/pagina_meus_lancamentos.js
@@ -30,24 +30,24 @@ function mudaCorAddR$() {
 }
 
 
-function mudaData() {
+function mudaCorAddR$() {
   var table = document.getElementById("tabela");
   for (var i = 1, row; (row = table.rows[i]); i++) {
-    for (var j = 2, col; (col = row.cells[j]); j += 4) {
-      data = col.innerHTML
-      let dataSeparada = data.split("/");
-       ; 
-
-        dataInvertida = dataSeparada
-          .reverse()
-          .toString()
-          .replace(",", "/")
-          .replace(",", "/");
-
-       col.innerHTML = dataInvertida;
+    for (var j = 1, col; (col = row.cells[j]); j += 4) {
+      valor = (col.innerHTML).replace(",",".");
+      if (valor >= 0) {
+        row.classList.add("green");
+        col.innerHTML = "R$ " + valor;
+      } else {
+        row.classList.add("red");
+        valor = valor * -1; 
+        col.innerHTML =
+        "- " + "R$ " + valor;
       }
+      
     }
   }
+}
 
 function semLancamentos(){
     let spam = document.querySelector('spam')
